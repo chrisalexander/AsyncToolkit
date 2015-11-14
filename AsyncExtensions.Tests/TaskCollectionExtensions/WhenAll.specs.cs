7fcc59174e10d7b31eb537f419323f16a0679144
Chris Alexander <chris@chris-alexander.co.uk>
2015-11-14 22:44:50 +0000

    Tests for task with return type

--- AsyncExtensions.Tests/TaskCollectionExtensions/WhenAll.specs.cs
+++ AsyncExtensions.Tests/TaskCollectionExtensions/WhenAll.specs.cs
@@ -60,5 +60,65 @@ namespace AsyncExtensions.Tests.TaskCollectionExtensions
             "Then the task should scomplete"
                 ._(() => allTask.IsCompleted.Should().BeTrue());
         }
+
+        [Scenario]
+        public void CompletedTasksWithType(
+            TaskCompletionSource<int> tcs1,
+            TaskCompletionSource<int> tcs2,
+            TaskCompletionSource<int> tcs3,
+            List<Task<int>> tasks,
+            Task<int[]> allTask)
+        {
+            "Given a set of task completion sources"
+                ._(() =>
+                {
+                    tcs1 = new TaskCompletionSource<int>();
+                    tcs2 = new TaskCompletionSource<int>();
+                    tcs3 = new TaskCompletionSource<int>();
+                });
+
+            "And given a collection of tasks from them"
+                ._(() =>
+                {
+                    tasks = new List<Task<int>>();
+
+                    tasks.Add(tcs1.Task);
+                    tasks.Add(tcs2.Task);
+                    tasks.Add(tcs3.Task);
+                });
+
+            "When we request an overall task"
+                ._(() =>
+                {
+                    allTask = tasks.WhenAll();
+                });
+
+            "Then the task should not be complete yet"
+                ._(() => allTask.IsCompleted.Should().BeFalse());
+
+            "When two of the tasks are completed"
+                ._(() =>
+                {
+                    tcs1.SetResult(1);
+                    tcs2.SetResult(2);
+                });
+
+            "Then the task should still not be completed"
+                ._(() => allTask.IsCompleted.Should().BeFalse());
+
+            "When the final task is completed"
+                ._(() => tcs3.SetResult(3));
+
+            "Then the task should scomplete"
+                ._(() => allTask.IsCompleted.Should().BeTrue());
+
+            "Then the results should be correct"
+                ._(() =>
+                {
+                    allTask.Result[0].ShouldBeEquivalentTo(1);
+                    allTask.Result[1].ShouldBeEquivalentTo(2);
+                    allTask.Result[2].ShouldBeEquivalentTo(3);
+                });
+        }
     }
 }
